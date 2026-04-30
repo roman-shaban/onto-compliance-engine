1a06b9be9389f2e432babfc52b271b6d3d6af2b0
Roman Shaban <systems.by.shaban@gmail.com>
2026-04-30 20:25:18 +0300

    docs: add copyright headers to all source files

--- test_reconciliation.py
+++ test_reconciliation.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2026 Roman Shaban. All rights reserved.
+# Licensed under the Apache License 2.0.
+# Part of the CIOS / Onto-Protocol Ecosystem.
 from reconciliation import reconcile_from_child
 
 # blocked/retried child from your latest run history
