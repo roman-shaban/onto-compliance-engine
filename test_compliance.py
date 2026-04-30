0e8c69d178649ab7bf6db97100e9444faeabb404
Roman Shaban <systems.by.shaban@gmail.com>
2026-04-30 20:23:53 +0300

    docs: add copyright headers to all source files

--- test_compliance.py
+++ test_compliance.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2026 Roman Shaban. All rights reserved.
+# Licensed under the Apache License 2.0.
+# Part of the CIOS / Onto-Protocol Ecosystem.
 from recovery import load_task_by_id
 from compliance_engine import (
     evaluate_task_compliance,
