e2bfb96c77f2daf8a302523825c59b018f796565
Roman Shaban <systems.by.shaban@gmail.com>
2026-04-30 20:26:02 +0300

    docs: add copyright headers to all source files

--- test_recovery.py
+++ test_recovery.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2026 Roman Shaban. All rights reserved.
+# Licensed under the Apache License 2.0.
+# Part of the CIOS / Onto-Protocol Ecosystem.
 from recovery import show_task_summary
 
 # Встав сюди task_id з tasks.json
