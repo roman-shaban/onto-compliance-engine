96c19530b48b6e86d918d22a2f9a12f0ea3b6656
Roman Shaban <systems.by.shaban@gmail.com>
2026-04-30 20:27:03 +0300

    docs: add copyright headers to all source files

--- test_resume.py
+++ test_resume.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2026 Roman Shaban. All rights reserved.
+# Licensed under the Apache License 2.0.
+# Part of the CIOS / Onto-Protocol Ecosystem.
 from recovery import resume_task
 
 # blocked task from your storage/tasks.json
